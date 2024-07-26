a783e99bfff4d2c3039fba4e94b2d6ff46321169
Jose Copeti <jrcopeti@gmail.com>
2024-07-26 14:55:16 +0200

    port in production

--- src/services/apiRecipes.js
+++ src/services/apiRecipes.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const apiURL = "http://localhost:5005";
+const apiURL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_URL_PRODUCTION
+    : import.meta.env.VITE_API_URL_DEVELOPMENT;
 
 const getAllRecipesApi = async () => {
   try {
