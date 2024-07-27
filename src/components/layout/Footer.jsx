fc5ad8e706bf1d6b90fbe62c0219c9cf4711bf11
Jose Copeti <jrcopeti@gmail.com>
2024-07-27 13:57:43 +0200

    fixed layout

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,15 +3,27 @@ function Footer() {
     <footer className="mt-auto border-2 border-pallette-50 bg-pallette-400 p-4 text-center text-2xl font-normal text-pallette-200">
       <p>
         &copy; 2024 Created by{" "}
-        <a href="https://github.com/jrcopeti" target="_blank">
+        <a
+          href="https://github.com/jrcopeti"
+          target="_blank"
+          className="transform text-pallette-500 duration-200 ease-in-out hover:text-pallette-200"
+        >
           Jose Copeti
         </a>
         ,{" "}
-        <a href="https://github.com/lamadelpolya" target="_blank">
+        <a
+          href="https://github.com/lamadelpolya"
+          target="_blank"
+          className="transform text-pallette-500 duration-200 ease-in-out hover:text-pallette-200"
+        >
           Polina Yukhymenko
         </a>
         ,{" "}
-        <a href="https://github.com/Ole-Guacamole" target="_blank">
+        <a
+          href="https://github.com/Ole-Guacamole"
+          target="_blank"
+          className="transform text-pallette-500 duration-200 ease-in-out hover:text-pallette-200"
+        >
           Sven Ole Spindler
         </a>
         . All rights reserved.
